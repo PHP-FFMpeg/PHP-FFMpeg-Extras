336b83e33413c12f62512d5121145d85b761020c
Romain Neutron <imprec@gmail.com>
2012-06-06 13:11:24 +0200

    Add unit tests

---
+++ tests/src/FFMpeg/Format/Video/ThreeGPTest.php
@@ -0,0 +1,247 @@
+<?php
+
+namespace FFMpeg\Format\Video;
+
+class ThreeGPTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getWidth
+     * @todo Implement testGetWidth().
+     */
+    public function testGetWidth()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::setWidth
+     * @todo Implement testSetWidth().
+     */
+    public function testSetWidth()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getHeight
+     * @todo Implement testGetHeight().
+     */
+    public function testGetHeight()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::setHeight
+     * @todo Implement testSetHeight().
+     */
+    public function testSetHeight()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getKiloBitrate
+     * @todo Implement testGetKiloBitrate().
+     */
+    public function testGetKiloBitrate()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getExtraParams
+     * @todo Implement testGetExtraParams().
+     */
+    public function testGetExtraParams()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getAudioSampleRate
+     * @todo Implement testGetAudioSampleRate().
+     */
+    public function testGetAudioSampleRate()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getComputedDimensions
+     * @todo Implement testGetComputedDimensions().
+     */
+    public function testGetComputedDimensions()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getVideoCodec
+     * @todo Implement testGetVideoCodec().
+     */
+    public function testGetVideoCodec()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getAudioCodec
+     * @todo Implement testGetAudioCodec().
+     */
+    public function testGetAudioCodec()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getAvailableVideoCodecs
+     * @todo Implement testGetAvailableVideoCodecs().
+     */
+    public function testGetAvailableVideoCodecs()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getAvailableAudioCodecs
+     * @todo Implement testGetAvailableAudioCodecs().
+     */
+    public function testGetAvailableAudioCodecs()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getFrameRate
+     * @todo Implement testGetFrameRate().
+     */
+    public function testGetFrameRate()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::setKiloBitrate
+     * @todo Implement testSetKiloBitrate().
+     */
+    public function testSetKiloBitrate()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::setAudioSampleRate
+     * @todo Implement testSetAudioSampleRate().
+     */
+    public function testSetAudioSampleRate()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::setFrameRate
+     * @todo Implement testSetFrameRate().
+     */
+    public function testSetFrameRate()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::setVideoCodec
+     * @todo Implement testSetVideoCodec().
+     */
+    public function testSetVideoCodec()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::setAudioCodec
+     * @todo Implement testSetAudioCodec().
+     */
+    public function testSetAudioCodec()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::getGOPSize
+     * @todo Implement testGetGOPSize().
+     */
+    public function testGetGOPSize()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers FFMpeg\Format\Video\ThreeGP::supportBFrames
+     * @todo Implement testSupportBFrames().
+     */
+    public function testSupportBFrames()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
